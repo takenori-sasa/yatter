4ce24e082f30e1b90afc54005e56e0de9a3de886
takenori-sasa <yoshida.tkw@gmail.com>
2023-08-04 14:18:57 +0900

    👍# ddlにrelationship追加

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -11,10 +11,19 @@ CREATE TABLE `account` (
 );
 CREATE TABLE `status` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-`account_id` bigint NOT NULL,
+  `account_id` bigint NOT NULL,
   `content` text NOT NULL,
   `url` text,
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-INDEX idx_account_id (account_id),   CONSTRAINT fk_status_account_id FOREIGN KEY (account_id) REFERENCES  account (id)
+  INDEX idx_account_id (account_id),
+  CONSTRAINT fk_status_account_id FOREIGN KEY (account_id) REFERENCES  account (id) ON DELETE CASCADE
+);
+CREATE TABLE `relationship` (
+  `following_id` bigint(20) NOT NULL,
+  `followed_id` bigint(20) NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`following_id`, `followed_id`),
+  CONSTRAINT `fk_relationship_following_id` FOREIGN KEY (`following_id`) REFERENCES `account` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_relationship_followed_id` FOREIGN KEY (`followed_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
 );
